5ed25138bd5793c1957d25ad278a4de5112028b1
Lilyy2565 <lilys2565@gmail.com>
2026-02-09 02:20:17 +0000

    Fix default `DebugHudEntryVisibility`

--- src/client/java/com/lilyy2565/hypixelautotip/mixin/client/DebugScreenEntriesMixin.java
+++ src/client/java/com/lilyy2565/hypixelautotip/mixin/client/DebugScreenEntriesMixin.java
@@ -3,15 +3,27 @@ package com.lilyy2565.hypixelautotip.mixin.client;
 import com.lilyy2565.hypixelautotip.HypixelAutoTipDebugEntry;
 import net.minecraft.client.gui.hud.debug.DebugHudEntries;
 import net.minecraft.client.gui.hud.debug.DebugHudEntry;
+import net.minecraft.client.gui.hud.debug.DebugHudEntryVisibility;
+import net.minecraft.client.gui.hud.debug.DebugProfileType;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Mixin(DebugHudEntries.class)
 public abstract class DebugScreenEntriesMixin {
+    
+    @Shadow
+    @Final
+    @Mutable
+    private static Map<DebugProfileType, Map<Identifier, DebugHudEntryVisibility>> PROFILES;
     
     @Shadow
     public static Identifier register(Identifier id, DebugHudEntry entry) {
@@ -20,6 +32,16 @@ public abstract class DebugScreenEntriesMixin {
     
     @Inject(method = "<clinit>", at = @At(value = "RETURN"))
     private static void registerHypixelAutoTipDebug(CallbackInfo ci) {
-        register(Identifier.of("hypixelautotip", "debug"), new HypixelAutoTipDebugEntry());
+        Identifier entryId = Identifier.of("hypixelautotip", "debug");
+        register(entryId, new HypixelAutoTipDebugEntry());
+        
+        // Set default visibility to "In Overlay" for all profiles
+        Map<DebugProfileType, Map<Identifier, DebugHudEntryVisibility>> updatedProfiles = new HashMap<>();
+        for (Map.Entry<DebugProfileType, Map<Identifier, DebugHudEntryVisibility>> profileEntry : PROFILES.entrySet()) {
+            Map<Identifier, DebugHudEntryVisibility> visibilityMap = new HashMap<>(profileEntry.getValue());
+            visibilityMap.putIfAbsent(entryId, DebugHudEntryVisibility.IN_F3);
+            updatedProfiles.put(profileEntry.getKey(), visibilityMap);
+        }
+        PROFILES = updatedProfiles;
     }
 }
