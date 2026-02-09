1b2c99c627862b3d6af11e5631386f73e1042348
Lilyy2565 <lilys2565@gmail.com>
2026-02-09 00:36:21 +0000

    Fix #2

--- src/client/java/com/lilyy2565/hypixelautotip/mixin/client/AutoTipDebugHudMixin.java
+++ src/client/java/com/lilyy2565/hypixelautotip/mixin/client/AutoTipDebugHudMixin.java
@@ -6,6 +6,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(DebugHud.class)
@@ -14,8 +15,10 @@ public class AutoTipDebugHudMixin {
     @ModifyVariable(method = "method_51745", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private List<String> addAutoTipInfo(List<String> debugInfo, net.minecraft.client.gui.DrawContext context, List<String> text, boolean left) {
         if (left) {
+            List<String> modifiedDebugInfo = new ArrayList<>(debugInfo);
             List<String> modInfo = HypixelAutoTipClient.getDebugInfo();
-            debugInfo.addAll(modInfo);
+            modifiedDebugInfo.addAll(modInfo);
+            return modifiedDebugInfo;
         }
         return debugInfo;
     }
